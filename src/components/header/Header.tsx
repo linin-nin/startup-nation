94e772c2aa56cc8c17e4e151cdd29e835802c14f
Liight9090 <liight9090@gmail.com>
2023-10-27 14:19:35 +0700

    update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,28 +5,25 @@ import SideBar from './SideBar'
 
 const Header = () => {
     return (
-        <header className='padding-body font-body border-b-2 border-white sticky top-0 bg-black z-[999]'>
+        <header className='container font-body border-b-2 border-white sticky top-0 bg-black z-[999]'>
             <nav className=' flex justify-between items-center'>
-                <button className='button-style text-[20px] font-bold'>Apply</button>
-                <div className="logo mr-12">
+                <button className='button-style text-[14px] font-bold sm:text-[20px]'>Apply</button>
+                <div className="logo mr-12 ">
                     <Image
                         src="/icons/logo.svg"
-                        width={200}
-                        height={80}
+                        width={0}
+                        height={0}
                         alt="Startup Nation"
-                        className="py-4"
+                        className=' ml-[20px] w-[200px] h-[100px] sm:w-[500px] sm:h-[100px]'
                     />
-
-
                 </div>
-                <div className="burger-menu">
-                    {/* <Burgermenu/> */}
-                    <SideBar/>
+                <div className="">
+                    <SideBar />
                 </div>
             </nav>
         </header>
 
-        
+
     )
 }
 
