7d40c2f4089396c47d4e7e41ccdf492405ba983f
Ninn04 <lynin722@gmail.com>
2023-10-30 13:19:27 +0700

    update About folder

--- src/components/About/intro.tsx
+++ src/components/About/intro.tsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player'
 
 const Intro = () => {
   return (
-    <main className='flex  justify-start border-b-2 border-white pt-5'>
+    <main className='mx-20 flex justify-start border-b-2 border-white pt-5'>
       <h2 className='title'>ABOUT<br/><span className='text-yellow-400'>STARTUPNATION LAB</span></h2>
       <div className='ml-[80px] relative before:absolute before:w-[2px] before:h-[100%] before:top-0 before:l-0 before:bg-white'>
         <div  className='pl-7 pb-7'>
@@ -11,7 +11,7 @@ const Intro = () => {
             StartupNation is a flagship community program of DreamsLAB for boot camp, heckathon, incubation, accelerator and investing.
           </h2>
           <div className='w-full h-[500px] bg-yellow-400'>
-            <ReactPlayer url='/image/vid1.mp4' playing={true} loop={true} controls width="100%" height="100%"/>
+            <ReactPlayer url='/image/vid4.mp4' playing={true} loop={true} controls width="100%" height="100%"/>
           </div>
         </div>
       </div>
