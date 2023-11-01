b2a700101250080ad2e0150cdf995105d1e0ac7a
Liight9090 <liight9090@gmail.com>
2023-11-01 09:40:35 +0700

    update directory grid

--- src/components/directory/Search.tsx
+++ src/components/directory/Search.tsx
@@ -2,13 +2,13 @@
 import Image from "next/image"
 import { Cards } from "@/lib/card-directory"
 import Link from "next/link"
-import Pagination from '@mui/material/Pagination'; 
-import {useState} from "react"
+import Pagination from '@mui/material/Pagination';
+import { useState } from "react"
 
 
 const Search = () => {
   const [pages, setpages] = useState(1)
-  const handleChange = (e:React.ChangeEvent<unknown>, p:number) =>{
+  const handleChange = (e: React.ChangeEvent<unknown>, p: number) => {
     setpages(p)
   }
 
@@ -52,35 +52,46 @@ const Search = () => {
         </div>
       </div>
       {/* ----grid--- */}
-      <div className="grid grid-cols-3 gap-y-2 gap-x-10 mt-10 overflow-hidden container">
-        {Cards.map((item)=>(
-            <div key={item.id} className="card h-[460px] w-[414px] relative before:absolute before:bg-gray-400 before:h-[100vh] before:w-[2px]
-            before:top-0 before:left-[-20px]">
-                <div className=" h-[400px] relative bg-cover bg-center">
-                  <Image src={item.img} fill bg-white alt="image" className="w-full h-full object-cover"/>
-                </div>
-                <div className="flex justify-between">
-                  <div className="">
-                    <Link href="/startup-directory/detail"><h2 className="text-2xl hover:underline hover:text-blue-400">{item.name}</h2></Link>
-                    <p>{item.date}</p>
-                  </div>
-                  <p>0{item.id}/100</p>
-                </div>
+
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+
+        {Cards.map((item) => (
+          <div key={item.id} className="">
+            <div className="h-[416px] ">
+              <Image src={item.img} alt="image" width={416}
+                height={416} className="h-full w-full object-cover"
+              />
             </div>
-         ))}
+            {/* card content  */}
+            <div className="flex justify-between">
+              <div className="">
+                <Link href="/startup-directory/detail"><h2 className="text-2xl hover:underline hover:text-blue-400">{item.name}</h2></Link>
+                <p>{item.date}</p>
+              </div>
+              <p>0{item.id}/100</p>
+            </div>
+          </div>
+        ))}
+
       </div>
+
+
+
+
+
+
       {/* ---Pagination--- */}
       <div className="flex justify-between items-center mt-10 py-5 w-full border-t-2 border-gray-400">
         <div>1 -6 from 100</div>
-          <Pagination 
-            count={16} 
-            color="standard"
-            variant="outlined" 
-            shape="rounded" 
-            size="large"
-            onChange={handleChange}/>
-            <div>Show rows </div>
-        </div>
+        <Pagination
+          count={16}
+          color="standard"
+          variant="outlined"
+          shape="rounded"
+          size="large"
+          onChange={handleChange} />
+        <div>Show rows </div>
+      </div>
     </section>
   )
 }
