b400d310378b2eb0c4ed6228b19d3265e8350dfa
Sreynin <snin934@gmail.com>
2023-11-23 08:41:27 +0700

    fix bug PreLoading section

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Emptybox from "@/components/Emptybox";
 import EmailContactus from "@/components/contactUs/Emailcontactus";
 import HeroContactus from "@/components/contactUs/HeroContactus";
@@ -6,22 +8,25 @@ import ShowPath from "@/components/directory/showPath";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/Header";
 import React from "react";
+import Smooth from "@/components/smoothScroll";
 
 const ContactUs = () => {
   return (
-    <div>
-      <div className=" bg-black padding-body right-0 left-0 sticky top-0 z-50">
-        <Header />
+    <Smooth>
+      <div>
+        <div className=" bg-black padding-body right-0 left-0 sticky top-0 z-50">
+          <Header />
+        </div>
+        <div className="padding-body font-body">
+          <ShowPath />
+          <HeroContactus />
+          <Map />
+          <EmailContactus />
+          <Emptybox />
+          <Footer />
+        </div>
       </div>
-      <div className="padding-body font-body">
-        <ShowPath />
-        <HeroContactus />
-        <Map />
-        <EmailContactus />
-        <Emptybox />
-        <Footer />
-      </div>
-    </div>
+    </Smooth>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 'use client';
 import Header from '@/components/header/Header';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Hero from '@/components/Hero';
 import VideoContent from '@/components/VideoContent';
-import AboutUs from '@/components/AboutUs';
+import AboutUs from '@/components/aboutUs';
 import Footer from '@/components/footer/footer';
 import Smooth from '@/components/smoothScroll';
+import Prelanding from '@/components/preloading/PreLoading';
+import { AnimatePresence } from 'framer-motion';
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 1500);
+    })();
+  });
  
   return (
     <main className="container padding-body font-body">
+      <AnimatePresence>{isLoading && <Prelanding />}</AnimatePresence>
       <Smooth>
         <div>
           <Header/>
