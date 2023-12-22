5ed95563233b835b51709dee285b0fd5c7669d73
Sreynin <snin934@gmail.com>
2023-12-22 11:29:33 +0700

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           <Header />
           <MarqueeComponent />
           <Hero />
-          <VideoContent />
+          {/* <VideoContent /> */}
           <AboutUs />
           <Footer />
         </div>
