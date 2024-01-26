d58c9f8e9ba09901c33afbfdfd8d678fa0461620
Sreynin <snin934@gmail.com>
2024-01-26 08:50:51 +0700

    directory: delete getStartup.tsx, cate-filter.tsx, searchBar.tsx; startups: add page.tsx, layout.tsx, [query]; [query]: add page.tsx

---
+++ src/app/startups/[query]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Page hello</div>
+  )
+}
+
+export default Page
---
+++ src/app/startups/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react'
+import { Metadata } from 'next'
+import Header from '@/components/home/header/Header'
+import Hero from '@/components/home/Hero'
+import ShowPath from '@/components/directory/showPath'
+import Container from '@/components/container'
+import Footer from '@/components/footer/footer'
+
+
+const StartupsLayout = ({children}:{children: ReactNode}) => {
+  return (
+    <Container>
+        <Header/>
+        <Hero/>
+        <ShowPath/>
+        {children}
+        <Footer/>
+    </Container>
+  )
+}
+
+export default StartupsLayout
