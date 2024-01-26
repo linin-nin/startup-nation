58b1ff84e086b87b76b8fb59e4a492df7b737b09
Sreynin <snin934@gmail.com>
2024-01-26 14:42:49 +0700

    update

--- src/app/startups/layout.tsx
+++ src/app/startups/layout.tsx
@@ -14,33 +14,33 @@ const StartupsLayout = ({ children }: { children: ReactNode }) => {
     <Container>
       <Header />
       <Hero
-            cover="/image/la.gif"
-            title="Startup Directory"
-            subtitle="Lorem ipsum dolor sit amet consectetur. Venenatis nec sodales
+        cover="/image/la.gif"
+        title="Startup Directory"
+        subtitle="Lorem ipsum dolor sit amet consectetur. Venenatis nec sodales
               semper nec rhoncus leo arcu ultricies tincidunt."
-          />
-       <div
-      className={cn(
-        "lg:py-5 md:py-4 py-3 mb-5 border-b-2 border-t-2 font-body padding-body"
-      )}
-    >
-      <div className="inline-block">
-        <div className="flex justify-between gap-4">
-          <Link href="/">
-            <h2 className="sm:text-2xl text-[13px] hover:underline">Home</h2>
-          </Link>
-          <Image
-            src={"/icons/greater-then.svg"}
-            alt="greater-than-icon"
-            width={22}
-            height={22}
-          />
-          <h2 className="text-yellow-300 sm:text-2xl text-[13px]">
-            Startup Directory
-          </h2>
+      />
+      <div
+        className={cn(
+          "lg:py-5 md:py-4 py-3 mb-5 border-b-2 border-t-2 font-body padding-body"
+        )}
+      >
+        <div className="inline-block">
+          <div className="flex justify-between gap-4">
+            <Link href="/">
+              <h2 className="sm:text-2xl text-[13px] hover:underline">Home</h2>
+            </Link>
+            <Image
+              src={"/icons/greater-then.svg"}
+              alt="greater-than-icon"
+              width={22}
+              height={22}
+            />
+            <h2 className="text-yellow-300 sm:text-2xl text-[13px]">
+              Startup Directory
+            </h2>
+          </div>
         </div>
       </div>
-    </div>
       {children}
       <Footer />
     </Container>
